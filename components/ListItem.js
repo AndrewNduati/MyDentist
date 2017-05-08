1ea296e852cc37714613e7029706319ada1507de
AndrewNduati <andrew.njage@meltwater.org>
2017-05-08 00:32:34 +0000

    Add firebase db to index

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import ReactNative from 'react-native';
 const styles = require('../styles.js')
-const { View, TouchableHighlight, Text } = ReactNative; // What does this mean?? 
+const { View, TouchableHighlight, Text } = ReactNative;
 
 class ListItem extends Component {
+  
   render() {
     return (
       <TouchableHighlight onPress={this.props.onPress}>
         <View style={styles.li}>
-          <Text style={styles.liText}>{this.props.item.title}</Text>
+          <Text style={styles.liText}>{this.props.item.name}</Text>
+          <Text style={styles.liText}>{this.props.item.email}</Text>
         </View>
       </TouchableHighlight>
     );
--- index.android.js
+++ index.android.js
@@ -5,59 +5,99 @@
  */
 
 import React, { Component } from 'react';
+import FloatingActionButton from 'react-native-action-button';
+
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View,
   TouchableHighlight,
   ListView
 } from 'react-native';
 
-var Firebase =  require('firebase');
+var Firebase = require('firebase');
 const styles = require('./styles.js');
 const StatusBar = require('./components/StatusBar');
 const ListItem = require('./components/ListItem');
 
 var config = {
-  authDomain:'https://dentist-directory-1c7d3.firebaseio.com' // Get to post to firebase then continue.
+  databaseURL: 'https://dentist-directory-1c7d3.firebaseio.com' // Get to post to firebase then continue.
 }
-var myFirebaseRef = Firebase.initializeApp(config);
-  
+var myFirebaseApp = Firebase.initializeApp(config);
+
 export default class MyDentist extends Component {
- constructor(props) {
-  super(props);
-  this.state = {
-    dataSource: new ListView.DataSource({
+  constructor(props) {
+    super(props);
+    this.docsRef = myFirebaseApp.database().ref();
+    const dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
-    })
-  };
+    });
+
+    this.state = {
+      dataSource: dataSource,
+      newDoc: ""
+    };
+  }
+  listenForDocs(docsRef) {
+    docsRef.on('value', (dataSnapshot) => {
+      var docs = [];
+      dataSnapshot.forEach((child) => {
+        docs.push({
+          name: child.val().name,     
+          email: child.val().email,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(docs)
+      });
+    });
+  }
+
+_addDoc(){
+    if(this.state.newDoc === ""){ return;}
+
+    this.docsRef.push({name: this.state.newDoc});
+    this.setState({newDoc: ""});
 }
- _renderItem(item) {
+
+  _renderItem(item) {
     return (
-      <ListItem item="{item}"  />
+      <ListItem item={item} />
     );
   }
 
-componentDidMount() {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
-    })
+  componentDidMount() {
+    this.listenForDocs(this.docsRef); // Listen for updates from Firebase...
   }
-  
+
   render() {
+    console.log(this.props.dataSource)
     return (
       <View style={styles.container}>
-         <StatusBar title="My Dentist" />
-            <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) => <Text>{rowData.title}</Text>} // Just rendering title as opposed to entire ListView
-        
-          />
+        <StatusBar title="My Dentist" />
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.state.dataSource}
+          renderRow={(item) => this._renderItem(item)} // Just rendering title as opposed to entire ListView
+           />
+           <TextInput
+        value={this.state.newDoc}
+        style={styles.textEdit}
+        onChangeText={(text) => this.setState({newDoc: text})}
+        placeholder="New Doc"
+      />
+      <FloatingActionButton 
+        hideShadow={true} // this is to avoid a bug in the FAB library.
+        buttonColor="rgba(231,76,60,1)" 
+        onPress={this._addDoc.bind(this)}/>
       </View>
     );
   }
-  
+
 
 }
 
